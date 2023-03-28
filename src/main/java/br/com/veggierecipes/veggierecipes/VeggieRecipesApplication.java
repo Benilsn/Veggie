5923d14556c03bd58695274710f6d7a67fbdbbb2
Benilson <benilson.mtr@gmail.com>
2023-03-27 21:53:18 -0300

    template inicial

---
+++ src/main/java/br/com/veggierecipes/veggierecipes/VeggieRecipesApplication.java
@@ -0,0 +1,14 @@
+package br.com.veggierecipes.veggierecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeggieRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeggieRecipesApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/br/com/veggierecipes/veggierecipes/VeggieRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.veggierecipes.veggierecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeggieRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
